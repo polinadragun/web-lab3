724c00d3b2ea17a51a3d40dab2810c9f65ec660d
polinadragun <polinadragun2005@gmail.com>
2025-11-23 13:07:23 +0300

    paths changed

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import './sass/style.scss';
+
+import './js/header.js';
+import './js/footer.js';
+import './js/contact.js';
+
+import './js/blog.js';
+import './js/products.js';
+import './js/script.js';
+
+console.log("MAIN LOADED");
